707637caefd0c012c4fd747c799bb55021569310
Soumik Pal <soumikpal49@gmail.com>
2026-03-26 20:34:42 +0530

    Update IndexTest.php

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -4,12 +4,12 @@ use PHPUnit\Framework\TestCase;
 
 class IndexTest extends TestCase
 {
-    public function testIndexPhpOutputsHelloWorld()
+    public function testIndexPhpOutput()
     {
         ob_start();
         require __DIR__ . '/../index.php';
         $output = ob_get_clean();
 
-        $this->assertEquals("Hello World", $output);
+        $this->assertEquals("Hello CI/CD", $output);
     }
 }
